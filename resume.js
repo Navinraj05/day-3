8497c43ef59683586123f2b446943cfec5610ccc
Navinraj05 <115686558+Navinraj05@users.noreply.github.com>
2022-10-20 20:25:12 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,58 @@
+let myResume = {
+    "basics": {
+        "name": "Navin raj.N",
+        "email": "navinraj5712@gmail.com",
+        "phone": 910000000,
+        "degree": "B.E",
+        "location": {
+            "address": "xxxxx",
+            "postalCode": 600012,
+            "city": "Chennai",
+            "state": "Tamilnadu",
+            "country": "India"
+        },
+        "profiles": [
+            {
+
+                "github": "https://github.com/Navinraj05"
+            }
+        ]
+    },
+    "work": [
+        {
+            "company": "kotak securities",
+            "position": "Executive desktop suppout",
+            "startDate": "2022-06-01",
+            "endDate": "till",
+
+        },
+    ],
+    "education": [
+        {
+            "institution": "gojan school of business and technology",
+            "department": "ECE",
+            "studyType": "fulltime",
+            "batch start year": 2016,
+            "batch end year": 2020,
+            "gpa": 7.3,
+        }
+    ],
+    "skills": [
+        {
+            "name": "javascript",
+            "level": "beginer",
+
+        }
+    ],
+    "languages": [
+        {
+            "language": "Tamil,English",
+        }
+    ],
+    "interests": [
+        {
+            "name": "script writter,youtuber,",
+        }
+    ]
+}
+console.log(myResume);
